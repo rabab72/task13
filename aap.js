a95dc1ae332a875f37f214228b6d80dc01346fde
xahid1983 <ahmedibnhayder@gmail.com>
2024-02-23 13:30:42 +0500

    first commit

---
+++ aap.js
@@ -0,0 +1,149 @@
+// CHAPTER #1 -------------------
+
+// question-1
+alert("Welcome to Alert JS")
+
+// question-2
+alert("Error! Please enter a valid password")
+
+// question-3
+alert("Welcome to JS world \n Happy Coding!")
+
+// question-4
+alert("Welcome to Js Land...")
+alert("Happy Coding! \n Prevent this apge from creating additional dialogs.")
+
+// question-5
+alert("Hello ... I can run JS through my web browser's console")
+console.log("Hello ... I can run JS through my web browser's console")
+
+// question-6
+// alerts are used in this new html & css project.
+
+// question-7
+// scripts are used in body & head of this project.
+
+// CHAPTER #2 ---------------
+
+// question-1
+var username;
+
+// question-2
+var myname = "Rabab Ahmed";
+console.log(myname);
+
+// question-3
+var message = "Hello World!";
+alert(message);
+console.log(message);
+
+// question-4
+var firstname = "Jhone";
+var lastname = "Doe";
+var age = "15 years old";
+var institution = "Certificated Mobile Application Development";
+
+console.log(firstname + " " + lastname + " " + age + " " + institution);
+
+alert(firstname);
+alert(lastname);
+alert(age);
+alert(institution);
+
+// question-5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// question-6
+var email = "example@example.com";
+alert("My email address is  " + email);
+
+//question-7
+var book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from  the Book  " + book);
+
+//question-8
+document.write("Yah! I can write HTML content through JavaScript");
+
+// question-9
+const myString = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+console.log(myString);
+
+// CHAPTER #3 ---------------
+
+// question-1
+var age = 41;
+alert("I am " + age + "years old");
+
+// question-2
+var time = 14;
+alert("You have visited this site " + time + "times");
+
+// question-3
+var birthYear = 1983;
+var currentYear = new Date().getFullYear();
+var age = currentYear - birthYear;
+
+console.log(age);
+document.write("<br> You are " + age + " years old");
+
+// question-4
+var age = 41;
+alert("I am " + age + "years old");
+
+// question-5
+var visitorname = "John Doe";
+var Productquantity = "5 T-hirts(s)";
+var store = "XYZ Clothing Store";
+
+document.write("<br>" + visitorname + " " + "ordered" + " " + Productquantity + " " + " on" + " " + store);
+
+
+// CHAPTER #4 ---------------
+
+// question from (Alerts)
+// 1
+alert("First Name:")
+alert("Last Name:")
+alert("Email address:")
+alert("Password:")
+alert("Phone number:")
+
+// 2
+alert("You are learning JavaScript!")
+    // 3 
+alert("Please Explain this topic in class")
+
+// question from (variable)
+// 1
+var myVariableName = "value";
+alert(myVariableName);
+
+// 2
+var myVariable;
+myVariable = "This is a string assigned to myVariable.";
+console.log(myVariable);
+
+// 3
+var teamName = "Jawan Team";
+alert(teamName);
+
+// 4
+
+var bestMan = "Charlie";
+
+bestMan = "David";
+console.log(bestMan);
+
+// question from (numbers)
+// 1
+var caseQty;
+// value will be given later
+
+// 2
+caseQty = 144;
+alert(caseQty);
+
+// 3
+var num = 9;
+//  To use the variable num in a mathematical operation, you should declare it as a number (without quotes).
